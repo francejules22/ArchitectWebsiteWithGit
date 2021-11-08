2ec1fca952a8bcd636044ec8169a305733a2af21
Francis Jules Espartero <francisjulescelesteespartero@gmail.com>
2021-11-08 08:35:18 +0800

    Added scroll up event in script JS

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ document.querySelector(".hamburger-menu").addEventListener ("click" , () => {
   document.querySelector(".container").classList.toggle("change");
 });
 //End of Hamburger Menus
+
+//Scroll UP
+document.querySelector(".scroll-btn").addEventListener("click" , () =>{
+    document.querySelector("html").style.scrollBehavior ="smooth" ;
+    setTimeout(() =>{
+      document.querySelector("html").style.scrollBehavior ="unset" ;
+    },1000);
+});
+//End Of Scroll UP/
